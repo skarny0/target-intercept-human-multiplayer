ff7c6f2cfc9cbf98c046cb07eee6d937e9bf8ad4
Sheer Karny <skarny0@gmail.com>
2025-04-17 15:15:23 -0700

    trying again with a new database

--- src/mpconfig.js
+++ src/mpconfig.js
@@ -1,10 +1,21 @@
+// export const firebasempConfig = {
+//     apiKey: "AIzaSyCG0AFe989rOmJSgSF-kYfg1lJgR8l8_XA",
+//     authDomain: "multiplayer-dede1.firebaseapp.com",
+//     databaseURL: "https://multiplayer-dede1-default-rtdb.firebaseio.com",
+//     projectId: "multiplayer-dede1",
+//     storageBucket: "multiplayer-dede1.firebasestorage.app",
+//     messagingSenderId: "527702720193",
+//     appId: "1:527702720193:web:0b19e9959744792b949e40"
+// };
+
 export const firebasempConfig = {
-    apiKey: "AIzaSyCG0AFe989rOmJSgSF-kYfg1lJgR8l8_XA",
-    authDomain: "multiplayer-dede1.firebaseapp.com",
-    databaseURL: "https://multiplayer-dede1-default-rtdb.firebaseio.com",
-    projectId: "multiplayer-dede1",
-    storageBucket: "multiplayer-dede1.firebasestorage.app",
-    messagingSenderId: "527702720193",
-    appId: "1:527702720193:web:0b19e9959744792b949e40"
-};
+    apiKey: "AIzaSyDbLMHCIl9jITG9U0uwRdTJxOfFn-G02kg",
+    authDomain: "multiplayer-v2-81361.firebaseapp.com",
+    databaseURL: "https://multiplayer-v2-81361-default-rtdb.firebaseio.com",
+    projectId: "multiplayer-v2-81361",
+    storageBucket: "multiplayer-v2-81361.firebasestorage.app",
+    messagingSenderId: "723807308199",
+    appId: "1:723807308199:web:efc13a27042f16ad714e82"
+  };
+  
   
