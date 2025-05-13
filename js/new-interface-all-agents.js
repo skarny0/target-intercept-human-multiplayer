36bcce847263cba1e435fada2cf931e9391cfe1d
Sheer Karny <skarny0@gmail.com>
2025-05-13 15:05:39 -0700

    new db name for run2

--- js/new-interface-all-agents.js
+++ js/new-interface-all-agents.js
@@ -104,7 +104,7 @@ let studyId = 'placeHolder';
 if (DEBUG){
    studyId    = "multiplayer-main-0513-debug";
 } else {
-    studyId   = "multiplayer-main-0513";
+    studyId   = "multiplayer-main-0513-run2";
 }
 
 
